8123953184f92433d7cecb0ba1a9e61d5eeb20f4
Enea Jorgji <jorgji.enea@gmail.com>
2020-07-01 22:55:25 +0200

    added def test_add_to_cart_success in file test_item_page

--- item_page.py
+++ item_page.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-
 from home_page import HomePage
 
 
@@ -25,7 +24,13 @@ class ItemPage(HomePage):
         pass
 
     def add_to_chart_label(self):
-        pass
+        return self.find("#add-to-cart-button")
+
+    def added_to_chart_label(self):
+        return self.find("button._13q9y._8hkto.mp7g_f6.mnjl_0.mg9e_0.mvrt_0.mj7a_0.mh36_0.mg9e_0.mvrt_0.mj7a_0.mh36_0.mt3o_0")
+
+    def add_another_item(self):
+        return self.find("button._9a071_2tnYy._9a071_3I3EK._9a071_gW37L._9a071_3H9tX")
 
     def buy_now_label(self):
         pass
--- test_item_page.py
+++ test_item_page.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from driver import Driver
 from search_page import SearchPage
 from item_page import ItemPage
+from home_page import HomePage
 
 
 class TestItemPage(TestCase):
@@ -12,8 +13,30 @@ class TestItemPage(TestCase):
         self.driver.quit()
 
     def test_navigate(self):
-        page = self.__get_item_page(6442736754)
-        page.navigate()
+        self.page = self.__get_item_page(6442736754)
+        self.page.navigate()
 
     def __get_item_page(self, id):
         return ItemPage(self.driver, id)
+
+    def test_add_to_cart_success(self):
+        self.page = self.__get_item_page(6442736754)
+        self.page.navigate()
+        self.page.add_to_chart_label().click()
+
+        assert self.page.added_to_chart_label()
+
+    def test_multiple_add_to_cart_success(self):
+        self.page = self.__get_item_page(6442736754)
+        self.page.navigate()
+
+        for i in range(4):
+            self.page.add_another_item().click()
+
+        self.page.add_to_chart_label().click()
+
+        assert self.page.added_to_chart_label()
+
+        # for i in range(3):
+        #     self.page.search_many_additional_button().click()
+        #     self.page.search_many_input(2 + i).send_keys("aaa" + Keys.ESCAPE)
